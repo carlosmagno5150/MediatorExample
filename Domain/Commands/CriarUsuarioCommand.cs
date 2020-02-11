95ef16c7a3079fdfeca4a20d33e1b502911e87a1
Carlos <cmagno@gmail.com>
2020-02-11 00:38:17 -0300

    genesis]

---
+++ Domain/Commands/CriarUsuarioCommand.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Model;
+using Domain.Validation;
+using MediatR;
+
+namespace Domain.Commands
+{
+    public class CriarUsuarioCommand: IRequest<ResponseResult>
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public CriarUsuarioCommand(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
---
+++ Domain/Model/ResponseResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Model
+{
+    public class ResponseResult
+    {
+        public bool Success { get; set; }
+    }
+}
